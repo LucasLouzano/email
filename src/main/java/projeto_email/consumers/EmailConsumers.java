4528c59128eee1f929af88bc3b1d8b30d70e2041
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-23 22:14:04 -0300

    Implementalção da classe EmailConsumers

--- src/main/java/projeto_email/consumers/EmailConsumers.java
+++ src/main/java/projeto_email/consumers/EmailConsumers.java
@@ -1,2 +1,23 @@
-package projeto_email.consumers;public class EmailConsumers {
+package projeto_email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import projeto_email.dto.EmailDTO;
+import projeto_email.model.EmailModel;
+import projeto_email.service.EmailService;
+@Component
+public class EmailConsumers {
+    @Autowired
+    private EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDTO emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        EmailModel emailModel1 = emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel1.getStatusEmail().toString());
+    }
 }
