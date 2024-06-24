9e51e79c1f250cde2f448c3a5988b7683057c1b1
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-23 21:59:11 -0300

    Implementação do EmailService

---
+++ src/main/java/projeto_email/email/service/EmailService.java
@@ -0,0 +1,53 @@
+package projeto_email.email.service;
+
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import projeto_email.email.enums.StatusEmail;
+import projeto_email.email.model.EmailModel;
+import projeto_email.email.repository.EmailRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+public class EmailService {
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+    @Autowired
+    private EmailRepository repository;
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return repository.save(emailModel);
+        }
+    }
+
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return  repository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(UUID emailId) {
+        return repository.findById(emailId);
+    }
+}
