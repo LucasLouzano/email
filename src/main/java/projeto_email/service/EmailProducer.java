a94428d13d937d47f639d74d609b1e9bef4a78d9
LucasLouzano <lucas_louzano@hotmail.com>
2024-08-11 23:36:34 -0300

    Ajuste no EmailProducer

--- src/main/java/projeto_email/service/EmailProducer.java
+++ src/main/java/projeto_email/service/EmailProducer.java
@@ -1,2 +1,21 @@
-package projeto_email.service;public class EmailProducer {
+package projeto_email.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import projeto_email.dto.EmailDTO;
+@Service
+public class EmailProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Value("${spring.rabbitmq.queue}")
+    private String queue;
+
+    public void sendToQueue(EmailDTO emailDTO) {
+        rabbitTemplate.convertAndSend(queue, emailDTO);
+    }
 }
+
